670c55d8257a1e4298c7d94344828d8adb9cfc80
msickpaler <kenyadayoyoyon@gmail.com>
2023-03-08 18:21:14 +0900

    mantineUIを追加

--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  corePlugins: {
+    preflight: false,
+  },
   darkMode: "class",
   theme: {
     extend: {
